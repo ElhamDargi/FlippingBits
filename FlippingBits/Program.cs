fdb0852f49101929e84db09fe0978f332e37185c
ElhamDrg <elham.dargi@gmail.com>
2022-07-06 21:58:23 +0200

    Add project files.

---
+++ FlippingBits/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlippingBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var binary = Convert.ToString(9, 2).PadLeft(32);
+            var strNum2 = binary.Select(t => t == '1' ? "0" : "1").ToList();
+            string result = string.Join("", strNum2);
+            Console.WriteLine(Convert.ToInt64(result, 2));
+        }
+    }
+}
